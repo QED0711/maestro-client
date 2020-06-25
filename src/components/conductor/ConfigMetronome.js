47338634c76438f45b940e6b3b47789309b6f955
QED0711 <qdizon@gmail.com>
2020-06-24 20:37:47 -0400

    added meter to metronome

--- src/components/conductor/ConfigMetronome.js
+++ src/components/conductor/ConfigMetronome.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import socket from '../../helpers/socket'
 
@@ -12,6 +12,8 @@ import { Section } from '../ui';
 const ConfigMetronome = () => {
 
     const {state} = useContext(mainContext)
+    const [metered, setMetered] = useState(false)
+
 
     const handleStartClick = e => {
         let bpm = parseInt(document.getElementById("bpm").value)
@@ -20,15 +22,28 @@ const ConfigMetronome = () => {
         bpm = bpm < 1 ? 1 : bpm
         subdivision = subdivision < 1 ? 1 : subdivision
 
+        let meter;
+        if(metered){
+            const numerator = parseInt(document.getElementById("meter-numerator").value)
+            const denominator = parseInt(document.getElementById("meter-denominator").value)
+            meter = [numerator, denominator]
+        } 
+
         socket.emit("metronome", {
             bpm, 
             subdivision: subdivision,
-            sessionKey: state.sessionKey
+            sessionKey: state.sessionKey,
+            meter
         })
     }
 
     const handleStopClick = e => {
         socket.emit("stop", {sessionKey: state.sessionKey})
+    }
+
+    const handleMeterToggle = e => {
+        const val = e.target.checked;
+        setMetered(val)
     }
 
     return (
@@ -40,6 +55,24 @@ const ConfigMetronome = () => {
             <label htmlFor={"subdivision"}>Subdivision</label>
             <input id="subdivision" type="number" min="1" max="12" step="1" defaultValue="1" required />
             <br/>
+
+
+            <input id="metered-checkbox" type="checkbox" onChange={handleMeterToggle} />
+            <label htmlFor={"metered-checkbox"}>Metered</label>
+
+            <br/>
+
+            {
+                metered 
+                &&
+                <>
+                <input id="meter-numerator" type="number" min="1" defaultValue="4" step="1" />
+                <br/>
+                <input id="meter-denominator" type="number" min="1" defaultValue="4" step="1" />
+                <br/>
+                </>
+            }
+
             <button onClick={handleStartClick}>Start</button>
             <button onClick={handleStopClick}>Stop</button>
             
