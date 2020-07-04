aaf8b149f6d45f30e1965a5e9091bd09f57ccf00
QED0711 <qdizon@gmail.com>
2020-07-04 11:03:39 -0400

    addTempoAdjustment method to cueClass

--- src/helpers/cueCreator.js
+++ src/helpers/cueCreator.js
@@ -1,27 +1,28 @@
 
 const fs = require("fs");
+const { start } = require("repl");
 
 class Cue {
-    constructor(){
+    constructor() {
         this.cueSheet = {};
     }
 
-    append(cue, measure, options){
-        let {repeat, startMeasure, measures} = options;
+    append(cue, measure, options) {
+        let { repeat, startMeasure, measures } = options;
         repeat = repeat || 1;
         startMeasure = startMeasure || 1;
 
 
         this.cueSheet[cue] = this.cueSheet[cue] || []; // initialize the cue if not already added
 
-        if(!!measures){ // if the user indicated a measure list
+        if (!!measures) { // if the user indicated a measure list
 
-            for(let measureNum of measures){
-                this.cueSheet[cue].push({...measure, measureNum})
+            for (let measureNum of measures) {
+                this.cueSheet[cue].push({ ...measure, measureNum })
             }
 
         } else { // if the user just indicated a starting measure
-            for(let i = startMeasure; i < startMeasure + repeat; i++){
+            for (let i = startMeasure; i < startMeasure + repeat; i++) {
                 this.cueSheet[cue].push({
                     parts: ["*"],
                     measureNum: i,
@@ -32,7 +33,49 @@ class Cue {
 
     }
 
-    save(path="./cueSheet.json"){
+
+    addTempoAdjustment(cue, fromTempo, toTempo, options = {}) {
+
+        cue = this.cueSheet[cue]
+        let { startMeasure, numBeats } = options;
+
+        startMeasure = startMeasure ? startMeasure - 1 : 0;
+        numBeats = numBeats || 1;
+
+
+        const totalChange = toTempo - fromTempo;
+
+        const adjustment = totalChange / numBeats;
+
+        let currentMeasure = startMeasure;
+        let beatsLeft = numBeats;
+
+        for (let [i, measure] of Object.entries(cue)){
+            
+            if(i < startMeasure) continue;
+
+            if(beatsLeft >= measure.numBeats){
+                measure.tempoAdjustment = adjustment
+                beatsLeft -= measure.numBeats
+            } else {
+                measure.tempoAdjustment = adjustment
+                measure.stopAdjustment = beatsLeft;
+                beatsLeft = 0;
+            }
+
+            if(beatsLeft === 0) break;
+        }
+
+
+
+    }
+
+    addFermata(cue) {
+
+    }
+
+
+    save(path = "./cueSheet.json") {
         fs.writeFileSync(path, JSON.stringify(this.cueSheet))
     }
 }
@@ -42,10 +85,7 @@ class Cue {
 
 const cue = new Cue();
 
-cue.append("cue1", {
-    bpm: 60, 
-    subdivision: 4,
-    numBeats: 4,
-}, {repeat: 10})
+cue.append("cue1", { bpm: 60, subdivision: 4, numBeats: 4 }, { repeat: 10 })
+cue.addTempoAdjustment("cue1", 60, 120, { numBeats: 15, startMeasure: 2 })
 
 cue.save()
